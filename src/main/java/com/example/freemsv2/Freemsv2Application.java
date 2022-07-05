42c5842ac1d4b0f8c65ef0af19f65fb2e148cb85
tzx4ys <otabek.matqulov@gmail.com>
2022-07-05 19:21:36 +0500

    First Commit

---
+++ src/main/java/com/example/freemsv2/Freemsv2Application.java
@@ -0,0 +1,13 @@
+package com.example.freemsv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Freemsv2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Freemsv2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/freemsv2/Freemsv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.freemsv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Freemsv2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
